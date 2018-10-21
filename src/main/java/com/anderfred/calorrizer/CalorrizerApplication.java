38831af6d573fb2fdc5bcf91dec1fe4af6f8aaa6
Egor Belkov <Subgorn@gmail.com>
2018-10-21 21:14:48 +0700

    init

---
+++ src/main/java/com/anderfred/calorrizer/CalorrizerApplication.java
@@ -0,0 +1,12 @@
+package com.anderfred.calorrizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorrizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorrizerApplication.class, args);
+	}
+}
